f56952b78b05fa6c61efe0dbdf5abbcf0a20e00c
ndpnews <atelier.coding@gmail.com>
2016-02-29 07:07:57 +0100

    Changement du titre

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="content-type" content="text/html; charset=utf-8" />
-<title>Design Profesionals</title>
+<title>NDP News</title>
 <meta name="keywords" content="" />
 <meta name="description" content="" />
 <link href="styles.css" rel="stylesheet" type="text/css" />
