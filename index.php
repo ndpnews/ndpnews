efeb4f45c9e8ece912e5f3a5e2986e8bddd04cd9
ndpnews <atelier.coding@gmail.com>
2016-03-08 13:23:31 +0100

    Modfication d'un texte

--- index.php
+++ index.php
@@ -89,7 +89,7 @@
 
 
 			
-			<div>Icons made by <a href="http://www.freepik.com" title="Freepik">Freepik</a> from <a href="http://www.flaticon.com" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
+			<div>Icônes faites par <a href="http://www.freepik.com" title="Freepik">Freepik</a> de <a href="http://www.flaticon.com" title="Flaticon">www.flaticon.com</a> est licencié par <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
             Copyright  2016    <a href="#">Privacy Policy</a> | <a href="#">Terms of Use</a> | <a href="admin">Administration</a>  </div>
 
 
