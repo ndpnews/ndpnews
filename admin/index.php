96cf6bd78c367521d9065dc8d00e17133b6622e9
ndpnews <atelier.coding@gmail.com>
2016-03-14 16:44:33 +0100

    Changement mineur

--- admin/index.php
+++ admin/index.php
@@ -23,6 +23,7 @@
 		<a href="newArticle.php">Créer un nouvel article</a><br>
 		<a href="delArticle.php">Supprimer un article</a><br>
 		<a href="editArticle.php">Editer un article</a>
+		<a href="upload.php">Envoyer une photo</a>
 		
 	</body>
 </html>
