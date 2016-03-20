d2b5d0991b98a68552c78713f00ab6cd5c8b0c34
ndpnews <atelier.coding@gmail.com>
2016-03-20 19:30:42 +0100

    Bug

--- contact.php
+++ contact.php
@@ -45,14 +45,57 @@
 		<?php
 		if(isset($_POST['email']) AND isset($_POST['message']) and isset($_POST['nom']))
 		{
-			$to = 'admin@ndp-news.tk';
-			$subject = 'Contact ' . $_POST['nom'];
-			$message = $_POST['message'];
-			$headers = 'From: '.$_POST['email'] . "\r\n" .
-			'Reply-To: '.$_POST['email'] . "\r\n" .
-			'X-Mailer: PHP/' . phpversion();
+			
+			$mail = 'admin@ndp-news.tk'; // Déclaration de l'adresse de destination.
+			if (!preg_match("#^[a-z0-9._-]+@(hotmail|live|msn).[a-z]{2,4}$#", $mail)) // On filtre les serveurs qui rencontrent des bogues.
+			{
+				$passage_ligne = "\r\n";
+			}
+			else
+			{
+				$passage_ligne = "\n";
+			}
+			//=====Déclaration des messages au format texte et au format HTML.
+			$message_txt = $_POST['message'];
+			//==========
+			 
+			//=====Création de la boundary
+			$boundary = "-----=".md5(rand());
+			//==========
+			 
+			//=====Définition du sujet.
+			$sujet = "Contact ". $_POST['nom'];
+			//=========
+			 
+			//=====Création du header de l'e-mail.
+			$header = "From: \"".$_POST['nom']."\"<".$_POST['nom'].">".$passage_ligne;
+			$header.= "Reply-to: \"".$_POST['nom']."\"<".$_POST['nom'].">".$passage_ligne;
+			$header.= "MIME-Version: 1.0".$passage_ligne;
+			$header.= "Content-Type: multipart/alternative;".$passage_ligne." boundary=\"$boundary\"".$passage_ligne;
+			//==========
+			 
+			//=====Création du message.
+			$message = $passage_ligne."--".$boundary.$passage_ligne;
+			//=====Ajout du message au format texte.
+			$message.= "Content-Type: text/plain; charset=\"ISO-8859-1\"".$passage_ligne;
+			$message.= "Content-Transfer-Encoding: 8bit".$passage_ligne;
+			$message.= $passage_ligne.$message_txt.$passage_ligne;
+			//==========
+			$message.= $passage_ligne."--".$boundary.$passage_ligne;
+			//=====Ajout du message au format HTML
+			$message.= "Content-Type: text/html; charset=\"ISO-8859-1\"".$passage_ligne;
+			$message.= "Content-Transfer-Encoding: 8bit".$passage_ligne;
+			$message.= $passage_ligne.$message_html.$passage_ligne;
+			//==========
+			$message.= $passage_ligne."--".$boundary."--".$passage_ligne;
+			$message.= $passage_ligne."--".$boundary."--".$passage_ligne;
+			//==========
+			 
+			//=====Envoi de l'e-mail.
+			mail($mail,$sujet,$message,$header);
+			//==========
 
-			mail($to, $subject, $message, $headers);
+
 		}
 		
 		
