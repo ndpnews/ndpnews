5a825f3a87645dd70bc86dd973f467d897453d9a
ndpnews <atelier.coding@gmail.com>
2016-03-20 19:20:14 +0100

    Bug

--- contact.php
+++ contact.php
@@ -49,11 +49,10 @@
 			$subject = 'Contact ' . $_POST['nom'];
 			$message = $_POST['message'];
 			$headers = 'From: '.$_POST['email'] . "\r\n" .
-			$_POST['email'] . "\r\n" .
+			'Reply-To: '.$_POST['email'] . "\r\n" .
 			'X-Mailer: PHP/' . phpversion();
 
 			mail($to, $subject, $message, $headers);
-			echo 'Message envoyé';
 		}
 		
 		
