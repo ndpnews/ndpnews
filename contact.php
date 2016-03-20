3352b6ed9fc8d9f1462f6f286ee817cf91787f89
ndpnews <atelier.coding@gmail.com>
2016-03-20 18:38:33 +0100

    Bug

--- contact.php
+++ contact.php
@@ -1,6 +1,11 @@
 <?php
 	date_default_timezone_set('Europe/Paris');
 	setlocale(LC_TIME, "fr_FR");
+	<?php
+	
+			  include_once('admin/bdd.php');
+			  global $bdd;
+			  ?>
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <!--Code par Maxence BARROY -->
