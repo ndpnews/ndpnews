a06453f8d43e39503308e9fb2db465d122ade845
ndpnews <atelier.coding@gmail.com>
2016-03-20 18:44:56 +0100

    Bug

--- right.php
+++ right.php
@@ -9,9 +9,9 @@
 				
 				$donnees = $req1->fetch();
 				?><ul>
-					<li><input type="checkbox" id="c1" /><i class="fa fa-angle-double-right"></i><i class="fa fa-angle-double-down"></i><label for="c1"> <?php $dateTime = strtotime($donnees['date_fr']);$annee = strftime("%G", $dateTime);echo $annee;?></label>
+					<li><input class="invisible" type="checkbox" id="c1" /><i class="fa fa-angle-double-right"></i><i class="fa fa-angle-double-down"></i><label for="c1"> <?php $dateTime = strtotime($donnees['date_fr']);$annee = strftime("%G", $dateTime);echo $annee;?></label>
 						<ul>
-							<li><input type="checkbox" id="c2" /><i class="fa fa-angle-double-right"></i><i class="fa fa-angle-double-down"></i> <label for="c2"> <?php $dateTime = strtotime($donnees['date_fr']);$mois = strftime("%B", $dateTime);echo $mois;?></label>
+							<li><input class="invisible" type="checkbox" id="c2" /><i class="fa fa-angle-double-right"></i><i class="fa fa-angle-double-down"></i> <label for="c2"> <?php $dateTime = strtotime($donnees['date_fr']);$mois = strftime("%B", $dateTime);echo $mois;?></label>
 								<ul>
 									<li><b><?php
 												$dateTime = strtotime($donnees['date_fr']);
@@ -43,7 +43,7 @@
 							//Si c'est pas le même mois
 							//Mettre le nouveau mois puis l'article
 						{
-							?></ul></li><li><input type="checkbox" id="c<?php echo $nC;?>" /><i class="fa fa-angle-double-right"></i><i class="fa fa-angle-double-down"></i> <label for="c<?php echo $nC;?>"> <?php $nC = $nC + 1;$dateTime = strtotime($donnees['date_fr']);$mois = strftime("%B", $dateTime);echo $mois;?></label>
+							?></ul></li><li><input class="invisible" type="checkbox" id="c<?php echo $nC;?>" /><i class="fa fa-angle-double-right"></i><i class="fa fa-angle-double-down"></i> <label for="c<?php echo $nC;?>"> <?php $nC = $nC + 1;$dateTime = strtotime($donnees['date_fr']);$mois = strftime("%B", $dateTime);echo $mois;?></label>
 							<ul><li><b><?php
 												$dateTime = strtotime($donnees['date_fr']);
 												$date = strftime("%d", $dateTime);
@@ -55,8 +55,8 @@
 					else
 					{
 						//Si c'est pas la même année?>
-						</ul></li></ul></li><li><input type="checkbox" id="c<?php echo $nC;?>" /><i class="fa fa-angle-double-right"></i><i class="fa fa-angle-double-down"></i> <label for="c<?php echo $nC;?>"> <?php $nC = $nC + 1; $dateTime = strtotime($donnees['date_fr']);$annee = strftime("%G", $dateTime);echo $annee;?></label><ul>
-						<li><input type="checkbox" id="c<?php echo $nC;?>" /><i class="fa fa-angle-double-right"></i><i class="fa fa-angle-double-down"></i> <label for="c<?php echo $nC;?>"> <?php $nC = $nC + 1; $dateTime = strtotime($donnees['date_fr']);$mois = strftime("%B", $dateTime);echo $mois;?></label><ul>
+						</ul></li></ul></li><li><input class="invisible" type="checkbox" id="c<?php echo $nC;?>" /><i class="fa fa-angle-double-right"></i><i class="fa fa-angle-double-down"></i> <label for="c<?php echo $nC;?>"> <?php $nC = $nC + 1; $dateTime = strtotime($donnees['date_fr']);$annee = strftime("%G", $dateTime);echo $annee;?></label><ul>
+						<li><input class="invisible" type="checkbox" id="c<?php echo $nC;?>" /><i class="fa fa-angle-double-right"></i><i class="fa fa-angle-double-down"></i> <label for="c<?php echo $nC;?>"> <?php $nC = $nC + 1; $dateTime = strtotime($donnees['date_fr']);$mois = strftime("%B", $dateTime);echo $mois;?></label><ul>
 						<li><b><?php
 												$dateTime = strtotime($donnees['date_fr']);
 												$date = strftime("%d", $dateTime);
