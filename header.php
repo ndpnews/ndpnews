489f85fb66e3c9598858d838fe531b61faf2439a
ndpnews <atelier.coding@gmail.com>
2016-03-23 17:17:55 +0100

    Ajout de l'animation

--- header.php
+++ header.php
@@ -1,9 +1,9 @@
 <div id="logo"> <img  style="width:1009px;"src="images/logo.png"/>  </div>
     <div id="buttons">
-          <a href="index.php" class="but"  title="">Accueil</a>
-          <a href="catArticle.php"  class="but" title="">Articles</a>
-		  <a href="tuto.php" class="but" title="">Tutos</a>
-          <a href="#"  class="but" title="">Qui sommes-nous ?</a>
-          <a href="ideeArticle.php" class="but" title="">Proposer un article</a>
-		  <a href="contact.php" class="but" title="">Contact</a>
+          <a onClick="document.getElementById('main').style.webkitAnimationName = 'main3';" href="index.php" class="but"  title="">Accueil</a>
+          <a onClick="document.getElementById('main').style.webkitAnimationName = 'main3';" href="catArticle.php"  class="but" title="">Articles</a>
+		  <a onClick="document.getElementById('main').style.webkitAnimationName = 'main3';" href="tuto.php" class="but" title="">Tutos</a>
+          <a onClick="document.getElementById('main').style.webkitAnimationName = 'main3';" href="#"  class="but" title="">Qui sommes-nous ?</a>
+          <a onClick="document.getElementById('main').style.webkitAnimationName = 'main3';" href="ideeArticle.php" class="but" title="">Proposer un article</a>
+		  <a onClick="document.getElementById('main').style.webkitAnimationName = 'main3';" href="contact.php" class="but" title="">Contact</a>
     </div>
