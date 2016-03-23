7b86a07ad9c7a0225ce176972df980a3465ee102
ndpnews <atelier.coding@gmail.com>
2016-03-23 15:13:40 +0100

    Ajout du lien

--- header.php
+++ header.php
@@ -1,7 +1,7 @@
 <div id="logo"> <img  style="width:1009px;"src="images/logo.png"/>  </div>
     <div id="buttons">
           <a href="index.php" class="but"  title="">Accueil</a>
-          <a href="#"  class="but" title="">Articles</a>
+          <a href="catArticle.php"  class="but" title="">Articles</a>
 		  <a href="#" class="but" title="">Tutos</a>
           <a href="#"  class="but" title="">Qui sommes-nous ?</a>
           <a href="ideeArticle.php" class="but" title="">Proposer un article</a>
