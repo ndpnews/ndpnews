adb24a88ea499f83a9da4682aa8628e275e4fa15
ndpnews <atelier.coding@gmail.com>
2016-03-25 11:31:04 +0100

    Ajout du qui sommes-nous.php

--- header.php
+++ header.php
@@ -3,7 +3,7 @@
           <a onClick="document.getElementById('main').style.webkitAnimationName = 'main3';" href="index.php" class="but"  title="">Accueil</a>
           <a onClick="document.getElementById('main').style.webkitAnimationName = 'main3';" href="catArticle.php"  class="but" title="">Articles</a>
 		  <a onClick="document.getElementById('main').style.webkitAnimationName = 'main3';" href="tuto.php" class="but" title="">Tutos</a>
-          <a onClick="document.getElementById('main').style.webkitAnimationName = 'main3';" href="#"  class="but" title="">Qui sommes-nous ?</a>
+          <a onClick="document.getElementById('main').style.webkitAnimationName = 'main3';" href="quisommesnous.php"  class="but" title="">Qui sommes-nous ?</a>
           <a onClick="document.getElementById('main').style.webkitAnimationName = 'main3';" href="ideeArticle.php" class="but" title="">Proposer un article</a>
 		  <a onClick="document.getElementById('main').style.webkitAnimationName = 'main3';" href="contact.php" class="but" title="">Contact</a>
     </div>
